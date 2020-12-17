c55c2f81c9a5987e84e84be08d0e9f645371f05e
StephaneC <stephane.cavillon@itech.fr>
2020-12-17 09:19:06 +0100

    scroll doux et décalage de section sous navbar OK

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// scroll doux et decalage ne pas avoir le titre sous la navbar
+var navbartop = document.querySelector('.navbar');
+console.log(navbartop.clientHeight);
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        // problème avec le scroll to, ça marchait avec scrollintoview mais pasde décalage sous la barre de menu, 
+        // je pense que c'est un problème de selecteur
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            // top: (this.offsetTop - navbartop.clientHeight),
+            // left: 0,
+            behavior: 'smooth',
+        });
+    });
+});
